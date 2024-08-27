7089c99c68e59bd1e58539a43eeb410108dd3bf8
mihai133 <mihai.cngm@gmail.com>
2024-08-27 08:51:57 +0300

    Prettier code in style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,23 +1,23 @@
-
-import { Outlet } from "react-router-dom"
-import NavigationBar from "./Navigation/Navbar"
+import { Outlet } from "react-router-dom";
+import NavigationBar from "./Navigation/Navbar";
 // import Header from "./Header"
 // import Footer from "./Footer"
 
 export default function Layout() {
-    return (
-        <>
-          <main className="container-fluid row m-0 p-0">                 
-            {/* <Col md={2} className="d-none d-md-block p-0">
+  return (
+    <>
+      <main className="container-fluid row m-0 p-0">
+        {/* <Col md={2} className="d-none d-md-block p-0">
               <Sidebar />
             </Col>
             <Col  className="container-fluid m-0 p-0"> */}
-            <NavigationBar />
-                <Outlet />
+        <NavigationBar />
+        <div className="col-12 col-md-6 mx-auto mt-5 px-3">
+          <Outlet />
+        </div>
 
-            {/* </Col> */}
-            
-          </main>
-        </>
-    )
+        {/* </Col> */}
+      </main>
+    </>
+  );
 }
